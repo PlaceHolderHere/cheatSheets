35404d9cb1e8c75172c718eb72afc4adfc14e873
PlaceHolderHere <holderhereinplace@gmail.com>
2026-06-07 22:05:20 +0800

    Added TILE_SCALAR and SCALED_TILE_SIZE to GamePanel class and added Player Class with sprites in the resource file

--- Java-2d-game/src/main/GamePanel.java
+++ Java-2d-game/src/main/GamePanel.java
@@ -5,18 +5,14 @@ import java.awt.*;
 
 public class GamePanel extends JPanel implements Runnable{
     // CONSTANTS
-    final int TILE_SIZE = 48;
+    final public int TILE_SIZE = 16;
+    final public int TILE_SCALAR = 3;
+    final public  int SCALED_TILE_SIZE = TILE_SIZE * TILE_SCALAR;
     final int SCREEN_COLS = 16;
     final int SCREEN_ROWS = 9;
-    final int SCREEN_WIDTH = SCREEN_COLS * TILE_SIZE;
-    final int SCREEN_HEIGHT = SCREEN_ROWS * TILE_SIZE;
-    final Color BACKGROUNDCOLOR = Color.black;
-
-    // Object handling key inputs
-    KeyHandler keyH = new KeyHandler();
-
-    // game loop
-    Thread gameThread;
+    final int SCREEN_WIDTH = SCREEN_COLS * SCALED_TILE_SIZE;
+    final int SCREEN_HEIGHT = SCREEN_ROWS * SCALED_TILE_SIZE;
+    final Color BACKGROUNDCOLOR = Color.white;
 
     // Game Variables
     final int FPS = 60;
@@ -24,10 +20,14 @@ public class GamePanel extends JPanel implements Runnable{
     double nextDrawTime;
     double remainingTime;
 
-    // player variables
-    int playerX = 100;
-    int playerY = 100;
-    final int playerSpeed = 10;
+    // Object handling key inputs
+    KeyHandler keyH = new KeyHandler();
+
+    // Player
+    Player player = new Player(100, 100, 4, this);
+
+    // game loop
+    Thread gameThread;
 
     public GamePanel(){
         this.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
@@ -71,16 +71,16 @@ public class GamePanel extends JPanel implements Runnable{
     public void updateFrame(){
         // Key Presses
         if (keyH.upPressed){
-            playerY -= playerSpeed;
+            player.y -= player.speed;
         }
         if (keyH.downPressed){
-            playerY += playerSpeed;
+            player.y += player.speed;
         }
         if (keyH.leftPressed){
-            playerX -= playerSpeed;
+            player.x -= player.speed;
         }
         if (keyH.rightPressed){
-            playerX += playerSpeed;
+            player.x += player.speed;
         }
     }
 
@@ -88,11 +88,7 @@ public class GamePanel extends JPanel implements Runnable{
         super.paintComponent(g);
 
         Graphics2D g2 = (Graphics2D) g;
-
-        g2.setColor(Color.white);
-
-        // Parameters: x, y, width, height
-        g2.fillRect(playerX, playerY, TILE_SIZE, TILE_SIZE);
+        player.draw(g2);
 
         // Helps Save memory
         g2.dispose();
--- Java-2d-game/src/main/Player.java
+++ Java-2d-game/src/main/Player.java
@@ -1,2 +1,37 @@
-package main;public class Player {
+package main;
+
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.util.Objects;
+
+public class Player {
+    public int x, y;
+    public int speed;
+    GamePanel gp;
+    private BufferedImage sprite;
+
+    // Constructor
+    public Player(int x, int y, int speed, GamePanel gp){
+        this.x = x;
+        this.y = y;
+        this.speed = speed;
+        this.gp = gp;
+        getPlayerSprite();
+    }
+
+    public void getPlayerSprite() {
+        try {
+            sprite = ImageIO.read(Objects.requireNonNull(getClass().getResourceAsStream("/player/sprite_00.png")));
+        }
+        catch (IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void draw(Graphics2D g2){
+        g2.setColor(Color.white);
+        g2.drawImage(sprite, x, y, gp.SCALED_TILE_SIZE, gp.SCALED_TILE_SIZE, null);
+    }
 }
