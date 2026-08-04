62890ef106b34ac0b523370f49cebd91c1860743
PlaceHolderHere <holderhereinplace@gmail.com>
2026-08-04 23:58:55 +0800

    Added a basic create table function

--- MySQL-CheatSheet.sql
+++ MySQL-CheatSheet.sql
@@ -1,3 +1,17 @@
--- Create a Database 
+-- Create a Database --
 -- CREATE DATABASE [db_name] 
-CREATE DATABASE employees; 
+-- CREATE DATABASE employees; 
+
+-- Start Using the Database --
+-- USE [db_name]
+USE employees;
+
+-- Creating a Table
+-- CREATE TABLE [table_name] ([attribute name] [datatype],...);
+CREATE TABLE businesses(
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(200),
+    establish_date DATE,
+    num_of_employees INT
+);
+
