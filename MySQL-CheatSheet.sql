bfeb04e1739e3fcea5930d78cd580e4e28847776
PlaceHolderHere <holderhereinplace@gmail.com>
2026-08-06 22:08:38 +0800

    Added INSERT into MYSQL database

--- MySQL-CheatSheet.sql
+++ MySQL-CheatSheet.sql
@@ -26,3 +26,11 @@ CREATE TABLE employees(
     -- CONSTRAINT [constraint name] FOREIGN KEY ([col_name in current table]) REFERENCES [table]([column])
     CONSTRAINT fk_business_id FOREIGN KEY (business_id) REFERENCES businesses(id)
 );
+
+-- INSERT --
+-- Insert data into tables
+INSERT INTO businesses (id, name, establish_date, num_of_employees) VALUES (1, "Generic Fast Food", '2024-08-08', 20); -- Inserting one Value
+INSERT INTO businesses (id, name, establish_date, num_of_employees) VALUES -- Inserting Multiple Values at Once
+	(2, "Retail Store", '2023-02-15', 25),
+    (3, "Grocery Store", '2022-05-21', 35),
+    (4, "Clothing Store", '2025-12-08', 12); 
