af67616455eda5389a236fefe3ea2295b306ac5a
PlaceHolderHere <holderhereinplace@gmail.com>
2026-08-07 23:52:48 +0800

    Added ALTER to MySQL cheatsheet

--- MySQL-CheatSheet.sql
+++ MySQL-CheatSheet.sql
@@ -15,7 +15,7 @@ CREATE TABLE businesses(
     num_of_employees INT
 );
 
-CREATE TABLE employees(
+CREATE TABLE employee_info(
 	employee_id INT PRIMARY KEY,
     name VARCHAR(200),
     hourly_pay FLOAT,
@@ -26,6 +26,28 @@ CREATE TABLE employees(
     -- CONSTRAINT [constraint name] FOREIGN KEY ([col_name in current table]) REFERENCES [table]([column])
     CONSTRAINT fk_business_id FOREIGN KEY (business_id) REFERENCES businesses(id)
 );
+-- --------------------------------------------------------------------------------------------------------------------------
+
+-- ALTER --
+-- Allows you to change tables after they have been created
+
+-- RENAMING A TABLE
+ALTER TABLE employee_info RENAME TO employees;
+
+-- ADD
+ALTER TABLE employees ADD COLUMN email VARCHAR(200); -- Add a column
+ALTER TABLE employees ADD CONSTRAINT age_check CHECK (hourly_pay >= 7.25); -- Add a constraint to a column
+
+-- RENAME a column
+ALTER TABLE employees RENAME COLUMN name TO full_name; 
+
+-- MODIFY
+ALTER TABLE employees MODIFY name VARCHAR(256); -- Change a column's datatype and/or constraint/s
+ALTER TABLE employees MODIFY business_id INT AFTER employee_id; -- Change a column and move it
+
+-- DROP 
+ALTER TABLE employees DROP COLUMN age; -- Deletes a column in the database
+-- --------------------------------------------------------------------------------------------------------------------------
 
 -- INSERT --
 -- Insert data into tables
