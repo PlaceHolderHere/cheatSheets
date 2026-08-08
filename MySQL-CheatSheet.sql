044369fef2c84e214f357a70755a4fa89e81b2b4
PlaceHolderHere <holderhereinplace@gmail.com>
2026-08-08 23:24:51 +0800

    Added some more insert commands for employees table to MySQL-Cheatsheet

--- MySQL-CheatSheet.sql
+++ MySQL-CheatSheet.sql
@@ -56,3 +56,10 @@ INSERT INTO businesses (id, name, establish_date, num_of_employees) VALUES -- In
 	(2, "Retail Store", '2023-02-15', 25),
     (3, "Grocery Store", '2022-05-21', 35),
     (4, "Clothing Store", '2025-12-08', 12); 
+
+INSERT INTO employees (employee_id, full_name) VALUES (1, 'Person1');
+INSERT INTO employees (employee_id, business_id, full_name, hourly_pay, hire_date, email) VALUES (2, 1, 'Person2', 9.75, '2020-04-22', '2@email.com'),
+		(3, 3, 'Person3', 12.75, '2020-04-22', '3@email.com'),
+		(4, 3, 'Person4', 13.75, '2020-04-22', '4@email.com'),
+		(5, 1, 'Person5', 8.75, '2020-04-22', '5@email.com'),
+        (6, 4, 'Person6', 15.75, '2020-04-22', '6@email.com'); 
