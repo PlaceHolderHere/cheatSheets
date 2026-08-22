81f4fde50e91f4f96e1f210d4e604e851c631aca
PlaceHolderHere <holderhereinplace@gmail.com>
2026-08-22 17:40:18 +0800

    Added LIMIT to SELECT.sql in MYSQL cheatsheet

--- MySQLCheatSheet/4-SELECT(and-related-functions).sql
+++ MySQLCheatSheet/4-SELECT(and-related-functions).sql
@@ -1,17 +1,24 @@
--- --- USE --- -- 
+-- === USE === -- 
 -- Selects a database to use
 USE employees;
         
--- --- SELECT data/rows from a table --- -- 
+-- === SELECT data/rows from a table === -- 
 -- Syntax: SELECT [columns] FROM [table];
 SELECT * FROM employees; -- selects all columns
 SELECT employee_id, full_name FROM employees; -- selects only full_name and employee_id column
 -- --------------------------------------------------------------------------------------------------------------------------
    
--- --- ORDER BY --- --  
+-- === ORDER BY === --  
 -- Orders records based on a column
 SELECT * FROM employees ORDER BY hourly_pay;
 
 -- --- DESC (Descending Order) --- --
 SELECT * FROM employees ORDER BY hourly_pay DESC;
+-- --------------------------------------------------------------------------------------------------------------------------
+
+-- === LIMIT === --
+-- used to limit the number of records SELECT displays at once
+-- can be used with other functions such as WHERE, ORDER BY, etc.
+-- Syntax: SELECT * FROM [table] LIMIT [number];
+SELECT * FROM employees LIMIT 3;
 -- --------------------------------------------------------------------------------------------------------------------------
