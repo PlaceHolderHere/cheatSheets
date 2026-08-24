7b42254a23032c55791332315d21d3482725905d
PlaceHolderHere <holderhereinplace@gmail.com>
2026-08-24 22:12:30 +0800

    Added COUNT to MYSQL cheatsheet

---
+++ MySQLCheatSheet/14-FUNCTIONS.sql
@@ -0,0 +1,18 @@
+-- === USE === -- 
+-- Selects a database to use
+USE employees;
+-- --------------------------------------------------------------------------------------------------------------------------
+
+-- === FUNCTIONS === -- 
+-- Allows you to pass in parameters or "inputs" and receive a value in return
+-- Parameters refer to column/s, based on your query (so it can be affected by things like WHERE)
+-- ---------------------------------------------------------
+
+-- COUNT --
+-- counts the number of rows from a query
+SELECT COUNT(*) FROM employees;
+-- ---------------------------------------------------------
+
+
+
+-- --------------------------------------------------------------------------------------------------------------------------
