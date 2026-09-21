29e52bc9972e19cffaeb9b8c74a1af35f4d0bf2d
PlaceHolderHere <holderhereinplace@gmail.com>
2026-09-22 00:40:46 +0800

    Added ORDER BY under SELECT queries

---
+++ PostgreSQL/Scripts/4.SELECT/4.2.ORDER_BY.sql
@@ -0,0 +1,9 @@
+-- ORDER BY --
+	-- Allows you to sort a SELECT query based on the values in a given column
+	-- Syntax: [SELECT query] ORDER BY [col] [DESC or ASC];
+
+-- Ascending Order
+SELECT * FROM students ORDER BY birthday ASC;
+
+-- Descending Order
+SELECT * FROM students ORDER BY birthday DESC;
