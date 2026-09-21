f9e4dd18b6e9b945bb4590f3ca267aad271cb118
PlaceHolderHere <holderhereinplace@gmail.com>
2026-09-21 23:16:04 +0800

    Added Constraints to 2.CREATE_TABLE in PostgreSQL cheatsheets

--- PostgreSQL/Scripts/2.CREATE_TABLE.sql
+++ PostgreSQL/Scripts/2.CREATE_TABLE.sql
@@ -5,9 +5,14 @@
 --);
 -- Postgresql datatypes: https://www.postgresql.org/docs/current/datatype.html
 
+-- CONSTRAINTS
+	-- Specific requirements/checks that restrict allowed values in a column
+	-- Constraints Documentation: https://www.postgresql.org/docs/current/ddl-constraints.html
+
 CREATE TABLE students(
-	id INT,
-	first_name VARCHAR(50),
-	last_name VARCHAR(50),
+	id SERIAL PRIMARY KEY,
+	first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+	grade_level SMALLINT,
 	birthday DATE
 );
