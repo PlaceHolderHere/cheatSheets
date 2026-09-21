23230171e6150debfa81aa41cb92e42e5394185e
PlaceHolderHere <holderhereinplace@gmail.com>
2026-09-22 00:26:45 +0800

    Added basic SELECT statements to PostgreSQL cheatsheet

---
+++ PostgreSQL/Scripts/4.SELECT/4.1.SELECT.sql
@@ -0,0 +1,9 @@
+-- SELECT --
+	-- Allows you to retrieve data from a database based on certain conditions
+	-- General Syntax: SELECT [col], [col],...,[col] FROM [table] [conditions];
+
+-- Select all columns with (*) --
+SELECT * FROM students;
+
+-- Select specific columns --
+SELECT first_name, last_name FROM students;
