64afe54d1eed83b91e7fe43ebd52b32026617a80
PlaceHolderHere <holderhereinplace@gmail.com>
2026-09-26 01:13:27 +0800

    Added AGGREGATE_FUNCTION.sql to PostgreSQL cheatsheet

---
+++ PostgreSQL/Scripts/7.AGGREGATE_FUNCTIONS.sql
@@ -0,0 +1,23 @@
+-- AGGREGATE FUNCTIONS --
+-- Takes a set of rows as input, performs a specific function on a column/s in all of the rows, and returns a single value
+-- Syntax: [FUNCTION]([column])
+-- ex: COUNT(*) -> counts number of rows
+
+-- COUNT --
+-- counts the number of rows inputted
+SELECT COUNT(*) FROM students; 
+	-- This counts the number of students in the students table
+
+-- AVG --
+-- averages all of the values in a row in a single column
+SELECT AVG(gpa) FROM students;
+
+-- MAX --
+-- finds the highest value among all the rows
+SELECT MAX(gpa) FROM students;
+
+
+-- MIN --
+-- finds the lowest value among all the rows
+SELECT MIN(gpa) FROM students;
+
