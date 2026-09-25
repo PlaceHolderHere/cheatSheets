11d87eea4cb1bad63319bf760f65e4c2891e069c
PlaceHolderHere <holderhereinplace@gmail.com>
2026-09-25 23:51:51 +0800

    Added DISTINCT.sql to PostgreSQL cheatsheet

---
+++ PostgreSQL/Scripts/4.SELECT/4.4.DISTINCT.sql
@@ -0,0 +1,4 @@
+-- DISTINCT --
+-- returns only DISTINCT values from a column/s and removes duplicates
+-- SYNTAX: SELECT DISTINCT [col1], [optional col2]... FROM [table];
+SELECT DISTINCT grade_level FROM students;
